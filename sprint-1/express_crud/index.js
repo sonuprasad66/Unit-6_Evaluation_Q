430222b6753ae6ba3da27f1eb14f1caefac9fa45
sonuprasad66 <sp.srp1999@gmail.com>
2022-10-11 23:21:59 +0530

    express_crud_ip

--- sprint-1/express_crud/index.js
+++ sprint-1/express_crud/index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 app.use(express.json());
+const dns = require("dns");
+
+app.post("/getmeip", (req, res) => {
+  const url = req.body.website_name;
+
+  dns.resolve4(url, (err, address) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(address[0]);
+  });
+});
 
 app.get("/products", (req, res) => {
   const data = fs.readFileSync("./products.json", { encoding: "utf-8" });
@@ -51,7 +64,7 @@ app.patch("/products/:id", (req, res) => {
   const products = parseData.products;
 
   const updateData = products.map((item) => {
-    return item.id == id ? { ...payload, id: item.id } : item;
+    return item.id == id ? { ...payload } : item;
   });
 
   parseData.products = updateData;
@@ -65,13 +78,14 @@ app.patch("/products/:id", (req, res) => {
 
 app.put("/products/:id", (req, res) => {
   const { id } = req.params;
+  console.log(id);
   const payload = req.body;
   const data = fs.readFileSync("./products.json", { encoding: "utf-8" });
   const parseData = JSON.parse(data);
   const products = parseData.products;
 
   const updateData = products.map((item) => {
-    return item.id == id ? { ...payload, price: item.price } : item;
+    return item.id == id ? { ...payload } : item;
   });
 
   parseData.products = updateData;
@@ -82,22 +96,6 @@ app.put("/products/:id", (req, res) => {
 
   res.send("Thanks for puting");
 });
-
-
-// app.put("/products/:id", (req, res) => {
-//   const { id } = req.params;
-//   const data = fs.readFileSync(`./products.json`, { encoding: "utf-8" });
-//   const parseData = JSON.parse(data);
-//   const newData = req.body;
-//   parseData.products.map((item) => {
-//     item.id === id ? { ...item, newData } : item;
-//   });
-//   fs.writeFileSync("./products.json", JSON.stringify(parseData), {
-//     encoding: "utf-8",
-//   });
-
-//   res.send("put request sucessfull");
-// });
 
 app.listen(8080, () => {
   console.log("Listen in port 8080 successful");
